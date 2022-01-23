0098ff97d1cd3fff0066d9dafeddfd6a331e4105
srivatsavr02 <srivatsavr02@gmail.com>
2022-01-23 19:40:51 +0530

    checkout complete

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAt0Fj0TFF5kuPvz9JP63XNIW4bXWimXDw",
+    authDomain: "react-c4e51.firebaseapp.com",
+    projectId: "react-c4e51",
+    storageBucket: "react-c4e51.appspot.com",
+    messagingSenderId: "846510531869",
+    appId: "1:846510531869:web:7b55e7e14644c7bd2dadf4",
+    measurementId: "G-2CQLLZ09BQ"
+  };
